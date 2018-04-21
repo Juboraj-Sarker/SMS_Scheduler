5754f933ee0a78f0bbbf8b018ab5b59c12890254
Juboraj Sarker <juborajsarker001@gmail.com>
2018-04-22 01:34:50 +0600

    Used card view. Add progress bar on webview activity.

--- app/src/main/java/com/juborajsarker/smsscheduler/activity/DetailsActivity.java
+++ app/src/main/java/com/juborajsarker/smsscheduler/activity/DetailsActivity.java
@@ -14,7 +14,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.juborajsarker.smsscheduler.R;
 import com.juborajsarker.smsscheduler.java_class.DbHelper;
 import com.juborajsarker.smsscheduler.java_class.Scheduler;
@@ -49,6 +51,13 @@ public class DetailsActivity extends AppCompatActivity {
             actionBar.setDisplayShowHomeEnabled(true);
         }
 
+
+
+
+        MobileAds.initialize(getApplicationContext(), getString(R.string.banner_home_footer_1));
+        AdView mAdView = (AdView) findViewById(R.id.adView1);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("93448558CC721EBAD8FAAE5DA52596D3").build();
+        mAdView.loadAd(adRequest);
 
 
         messageTV = (TextView) findViewById(R.id.details_message_TV);
